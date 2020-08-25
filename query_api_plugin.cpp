4ce5add6c6da5a7fec74053050bd2f1df6ba4f72
liyukun <liyukun@starteos.io>
2020-08-25 09:21:39 +0800

    + fix problem

--- query_api_plugin.cpp
+++ query_api_plugin.cpp
@@ -28,9 +28,9 @@ public:
       };
    }
 
-   const void get_token_contracts( url_response_callback &&cb )
+   void get_token_contracts( url_response_callback &&cb ) const
    {
-      auto blog = _ctrl.block_log();
+      const auto &blog = _ctrl.block_log();
       auto first_block_num = blog.first_block_num();
       auto head_block_num = blog.head()->block_num();
       ilog( "scanning token contracts from block ${b} to block ${e}", ("b", first_block_num)("e", head_block_num) );
@@ -38,11 +38,11 @@ public:
       for ( auto i = first_block_num; i <= head_block_num; ++i )
       {
          const signed_block_ptr &block = blog->read_block_by_num( i );
-         for_each( block.transactions.begin(), block.transactions.end(), [&](const auto &v)
+         for_each( block->transactions.begin(), block->transactions.end(), [&](const auto &v)
          {
-            if ( v.trx.contains<packed_transaction>() )
+            if ( v.trx.template contains<packed_transaction>() )
             {
-               const auto &tx = v.trx.get<packed_transaction>().get_transaction();
+               const auto &tx = v.trx.template get<packed_transaction>().get_transaction();
                for_each( tx.actions.begin(), tx.actions.end(), [&](const auto &a)
                {
                   if ( a.name == N(transfer) )
@@ -70,7 +70,7 @@ void query_api_plugin::plugin_initialize(const variables_map& options) {}
 void query_api_plugin::plugin_startup() {
    ilog( "starting query_api_plugin" );
 
-   auto ctrl = app().get_plugin<chain_plugin>().chain();
+   const auto &ctrl = app().get_plugin<chain_plugin>().chain();
    my->reset( new query_api_plugin_impl(ctrl) );
    auto& _http_plugin = app().get_plugin<http_plugin>();
    _http_plugin.add_api({
